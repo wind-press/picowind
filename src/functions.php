db6e080409297eec5056ce9fb8fdc71fca78f36b
Joshua Gugun Siagian <suabahasa@gmail.com>
2025-09-18 19:09:44 +0700

    fix typehint

--- src/functions.php
+++ src/functions.php
@@ -27,8 +27,11 @@ function acf_block_render_callback(array $block, string $content = '', bool $is_
 
 /**
  * Get an instance of Symfony Finder, if available.
+ *
+ * @return null|\WindPressDeps\Symfony\Component\Finder\Finder|\Symfony\Component\Finder\Finder
+ * @mago-expect lint:return-type
  */
-function get_symfony_finder(): ?\Symfony\Component\Finder\Finder
+function get_symfony_finder()
 {
     if (class_exists('\WindPressDeps\Symfony\Component\Finder\Finder')) {
         return new \WindPressDeps\Symfony\Component\Finder\Finder();
